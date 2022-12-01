aebb788786a42629ede24bbc362943b51b1d1dde
Johannes Ptaszyk <johannes@ptaszyk.de>
2022-12-01 21:28:32 +0100

    ADD day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,27 @@
 fun main() {
+
+    fun getElvesCalories(input: List<String>) =
+        input.fold(mutableListOf()) { elves: MutableList<Int>, s: String ->
+            elves.apply {
+                when {
+                    isEmpty() || s.isBlank() -> add(0)
+                    s.isNotBlank() -> this[lastIndex] += s.toInt()
+                }
+            }
+        }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return getElvesCalories(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return getElvesCalories(input).sortedDescending().subList(0,3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 4)
+    check(part2(testInput) == 9)
 
     val input = readInput("Day01")
     println(part1(input))
